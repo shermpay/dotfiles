787fbae85fd7059707dc9fce3c996ae685c25451
Sherman Pay <shermanpay@google.com>
2022-12-24 11:55:36 -0800

    Fix emacs.d/setup.sh to be more cross platform (support OSX).
    I might migrate to python and have a simple bootstrap script for installing python.
    
    1. Support Linux/OSX/Windows
    2. Don't care about old python versions.
    3. Don't need to install to much, just configure zsh and emacs.

--- emacs.d/setup.sh
+++ emacs.d/setup.sh
@@ -7,9 +7,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mkdir -p "$HOME/.emacs.d/lisp/"
 
 for file in $DIR/lisp/*; do
-  ln -sf "$file" "$HOME/.emacs.d/lisp/$(basename $file)" -i
+  ln -sf -i "$file" "$HOME/.emacs.d/lisp/$(basename $file)"
 done
 
-ln -sf "$DIR/config.org" "$HOME/.emacs.d/config.org" -i
-ln -sf "$DIR/init.el" "$HOME/.emacs.d/init.el" -i
+ln -sf -i "$DIR/config.org" "$HOME/.emacs.d/config.org"
+ln -sf -i "$DIR/init.el" "$HOME/.emacs.d/init.el"
  
